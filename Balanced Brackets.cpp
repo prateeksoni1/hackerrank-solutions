19d9852bd445a1a3e415cb37eb3eceb7b94aa42b
Proforty7 <proforty7@gmail.com>
2018-07-21 21:42:03 +0530

    Two solutions added

---
+++ Balanced Brackets.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the isBalanced function below.
+string isBalanced(string s) {
+
+    stack<char> stk;
+    string ans;
+    for(int i=0; i<s.length(); i++) {
+        char ch = s[i];
+        // cout << ch << endl;
+        if (ch == '{' || ch == '[' || ch == '(') {
+            stk.push(ch);
+        } else {
+
+            if (stk.empty()) {
+                ans = "NO";
+                return ans;
+            }
+            char popped = stk.top();
+            stk.pop();
+            if (ch == '}' && popped == '{') {
+                continue;
+            } else if (ch == ']' && popped == '[') {
+                continue;
+            } else if (ch == ')' && popped == '(') {
+                continue;
+            } else {
+                ans = "NO";
+                return ans;
+                break;      
+            }
+        }
+    }
+    if (stk.empty()) {
+        ans = "YES";
+    } else {
+        ans = "NO";
+    }
+    // cout << ans << endl;
+
+    return ans;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string s;
+        getline(cin, s);
+
+        string result = isBalanced(s);
+        // cout << result << "\n";
+        fout << result << "\n";
+
+    }
+
+    fout.close();
+
+    return 0;
+}
